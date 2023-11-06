67dc46efb7609b1f3e0b862405d9434fe61e2178
GustavoMuller-S <128441294+GustavoMuller-S@users.noreply.github.com>
2023-11-06 02:14:34 -0300

    funcion de botones matematicos

--- scripts.js
+++ scripts.js
@@ -1,2 +1,73 @@
 /* BOTONES DE OPERACIONES MATEMATICAS */
 
+function btnSumar() {
+    // Obtener los valores ingresados por el usuario
+    var numeroUno = parseFloat(document.getElementById("primerNum").value);
+    var numeroDos = parseFloat(document.getElementById("segundoNum").value);
+
+    // Verificar si los valores ingresados son números válidos
+    if (isNaN(numeroUno) || isNaN(numeroDos)) {
+    alert("Por favor, ingrese números válidos.");
+    return;
+    }
+
+    // Calcular la suma
+    var suma = numeroUno + numeroDos;
+
+    // Mostrar el resultado
+    document.getElementById("resultado").textContent = suma;
+}
+
+function btnRestar() {
+    // Obtener los valores ingresados por el usuario
+    var numeroUno = parseFloat(document.getElementById("primerNum").value);
+    var numeroDos = parseFloat(document.getElementById("segundoNum").value);
+
+    // Verificar si los valores ingresados son números válidos
+    if (isNaN(numeroUno) || isNaN(numeroDos)) {
+    alert("Por favor, ingrese números válidos.");
+    return;
+    }
+
+    // Calcular la resta
+    var resta = numeroUno - numeroDos;
+
+    // Mostrar el resultado
+    document.getElementById("resultado").textContent = resta;
+}
+
+function btnMulti() {
+    // Obtener los valores ingresados por el usuario
+    var numeroUno = parseFloat(document.getElementById("primerNum").value);
+    var numeroDos = parseFloat(document.getElementById("segundoNum").value);
+
+    // Verificar si los valores ingresados son números válidos
+    if (isNaN(numeroUno) || isNaN(numeroDos)) {
+    alert("Por favor, ingrese números válidos.");
+    return;
+    }
+
+    // Calcular la multiplicacion
+    var multi = numeroUno * numeroDos;
+
+    // Mostrar el resultado
+    document.getElementById("resultado").textContent = multi;
+}
+
+function btnDivid() {
+    // Obtener los valores ingresados por el usuario
+    var numeroUno = parseFloat(document.getElementById("primerNum").value);
+    var numeroDos = parseFloat(document.getElementById("segundoNum").value);
+
+    // Verificar si los valores ingresados son números válidos
+    if (isNaN(numeroUno) || isNaN(numeroDos)) {
+    alert("Por favor, ingrese números válidos.");
+    return;
+    }
+
+    // Calcular la division
+    var divid = numeroUno / numeroDos;
+
+    // Mostrar el resultado
+    document.getElementById("resultado").textContent = divid;
+}
